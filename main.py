344a30e553a4d87967f716773cb493955a08825e
ggabri4 <73182826+ggabri4@users.noreply.github.com>
2024-03-20 10:37:47 +0100

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "TEST 3 Hello {}!".format(name)
+    return "Hello {}!".format(name)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
